25647c5980316df9b9647eade2d020e608251cb1
Ion Stici <ionsticidev@gmail.com>
2023-04-15 10:12:00 +0300

    Update

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -1,8 +1,14 @@
+import React from 'react';
 import styles from './../styles/Prompt.module.scss';
 import { currentUser } from '../state/Data';
 
 const Prompt = function (props) {
     const type = props.type;
+    const form = React.useRef(null);
+    const img = React.useRef(null);
+    const input = React.useRef(null);
+    const btn = React.useRef(null);
+    const els = [img, input, btn];
 
     let buttonText = '';
     let placeholderText = '';
@@ -33,11 +39,19 @@ const Prompt = function (props) {
     //     }
     // };
 
+    React.useEffect(() => {
+        setTimeout(() => form.current.classList.add(styles.scale), 1);
+        setTimeout(() => {
+            els.forEach(el => el.current.classList.add(styles.show));
+        }, 1);
+    }, []);
+
     return (
-        <form className={`${styles.form} ${styles.scale}`} onSubmit={event}>
+        <form className={`${styles.form} ${''}`} onSubmit={event} ref={form}>
             <img
                 className={styles.img}
                 src={currentUser.image.png}
+                ref={img}
                 width="32"
                 height="32"
                 alt="User Photo"
@@ -45,13 +59,16 @@ const Prompt = function (props) {
 
             <textarea
                 className={styles.input}
+                ref={input}
                 type="text"
                 placeholder={placeholderText}
                 defaultValue={content}
                 // onKeyDown={handleKeyDown}
             />
 
-            <button className={styles.btn}>{buttonText}</button>
+            <button className={styles.btn} ref={btn}>
+                {buttonText}
+            </button>
         </form>
     );
 };
